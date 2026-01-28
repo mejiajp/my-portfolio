7c5c9646d334e911e208b808bf893ad5a81206bc
mejia-1mt <mejiapaulo13@gmail.com>
2026-01-28 19:23:09 +0800

    added files for up animation for navigation content

--- app/utilities/goUp.tsx
+++ app/utilities/goUp.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import "@/app/styles/goUp.css";
 
-export default function goUp() {
-  return <div>goUp</div>;
+type goUpProps = {
+  mounted: boolean | null;
+  children: React.ReactNode;
+};
+export default function goUp({ mounted, children }: goUpProps) {
+  console.log(mounted, "IS OPEN");
+  return (
+    <div className="go-up-container">
+      <div className={`go-up ${mounted ? "go" : ""}`}>{children}</div>
+    </div>
+  );
 }
