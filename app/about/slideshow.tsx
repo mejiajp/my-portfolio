ef94b833699215eedd21dd7ea48500b032f83d04
mejia-1mt <mejiapaulo13@gmail.com>
2026-07-14 23:19:30 +0800

    general update

--- app/about/slideshow.tsx
+++ app/about/slideshow.tsx
@@ -165,7 +165,7 @@ const Slideshow = () => {
         <p>DOOM Scroller</p>
       </div>
 
-      <div className={`${styles.slider} hidden lg:block cursor-pointer z-10`}>
+      <div className={`${styles.slider} hidden lg:block  z-10`}>
         <ul>
           <li className={`left-0`}>-ABOUT-</li>
 
